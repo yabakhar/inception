411eaf4d110bbbf65c43adfc443e95121f2c9b38
yabakhar <youssef.abakhar52@gmail.com>
2023-04-02 21:39:33 +0000

    install and config wordpress

--- srcs/requirements/wordpress/conf/config.sh
+++ srcs/requirements/wordpress/conf/config.sh
@@ -1,8 +1,20 @@
 #!/bin/sh
 
-sed -i 's/database_name_here/'"$MYSQL_DATABASE"'/g' /var/www/wordpress/wp-config.php
-sed -i 's/username_here/'"$MYSQL_USER"'/g' /var/www/wordpress/wp-config.php
-sed -i 's/password_here/'"$MYSQL_PASSWORD"'/g' /var/www/wordpress/wp-config.php
-sed -i 's|'localhost'|mariadb|' /var/www/wordpress/wp-config.php
+if [ ! -d /run/php ]; then
+    mkdir -p /run/php
+    chmod -R 777 /run/php
+fi
 
-php-fpm7.3 -F
+if [ ! -d /var/www/wordpress ]; then
+    mkdir -p /var/www/wordpress
+    chmod -R 777 /var/www/wordpress
+    chown -R www-data:www-data /var/www/wordpress    
+    cd /var/www/wordpress
+    wp core download --allow-root
+    wp config create --allow-root --dbhost=mariadb --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD
+    wp core install --allow-root --url=localhost --title=yabakhar --admin_user=yabakhar --admin_password=yabakhar --admin_email=yabakhar@example.com --skip-email
+    sed -i 's|listen = /run/php/php7.3-fpm.sock|listen = 9000|' /etc/php/7.3/fpm/pool.d/www.conf
+fi
+
+
+/usr/sbin/php-fpm7.3 -F
