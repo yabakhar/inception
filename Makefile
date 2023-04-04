945022f8e97463f78e5aa2ed85963722369d49b4
yabakhar <youssef.abakhar52@gmail.com>
2023-04-04 04:59:33 +0000

    fix all issue

--- Makefile
+++ Makefile
@@ -1,9 +1,25 @@
-docker stop $(docker  ps  -q) 
 
-docker rmi -f $(docker image ls  -q)
+init:
+	mkdir -p /Users/yabakhar/Desktop/inception_volume/wordpress /Users/yabakhar/Desktop/inception_volume/mariadb
+up: 
+	docker-compose -f ./srcs/docker-compose.yaml up --build
+down:
+	docker-compose -f ./srcs/docker-compose.yaml down
+fclean:
+	- clean
+	- docker stop $(docker  ps  -q) 
+	- docker rmi -f $(docker ps -aq )
+	- docker rmi -f $(docker image ls  -q)
+	- docker volume rm srcs_wordpress
+	- docker volume rm srcs_DB 
+	- rm -rf /Users/yabakhar/Desktop/inception_volume/mariadb/*
+	- rm -rf /Users/yabakhar/Desktop/inception_volume/wordpress/*
+re: fclean init up
 
 
-docker build -t nginx .
-
- --- delete all 
-docker system prune -a
+wo: 
+	- docker exec -it wordpress bash
+ng: 
+	- docker exec -it nginx bash
+ma: 
+	- docker exec -it mariadb bash
--- srcs/requirements/wordpress/conf/config.sh
+++ srcs/requirements/wordpress/conf/config.sh
@@ -5,16 +5,17 @@ if [ ! -d /run/php ]; then
     chmod -R 777 /run/php
 fi
 
-if [ ! -f /var/www/wordpress/wp-config.php ]; then
+# if [ ! -f /var/www/wordpress/wp-config.php ]; then
     mkdir -p /var/www/wordpress
     chmod -R 777 /var/www/wordpress
     chown -R www-data:www-data /var/www/wordpress    
     cd /var/www/wordpress
     wp core download --allow-root
-    wp config create --allow-root --dbhost=mariadb --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD
+    # wp config create --allow-root --url=$DOMAIN_NAME --dbhost=mariadb --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD
+    wp config create --allow-root --url=localhost --dbhost=mariadb --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD
     wp core install --allow-root --url=localhost --title=yabakhar --admin_user=yabakhar --admin_password=yabakhar --admin_email=yabakhar@example.com --skip-email
-    sed -i 's|listen = /run/php/php7.3-fpm.sock|listen = 9000|' /etc/php/7.3/fpm/pool.d/www.conf
-fi
-
+    wp user create --allow-root avnn ann@exavmple.com --user_pass=Qwerty.123
+    sed -i 's|listen = /run/php/php7.3-fpm.sock|listen = 0.0.0.0:9000|g' /etc/php/7.3/fpm/pool.d/www.conf
+# fi
 
 /usr/sbin/php-fpm7.3 -F
